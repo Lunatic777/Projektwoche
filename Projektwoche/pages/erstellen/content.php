597ff764d41c78577e2682817656155cd2a09463
Projektwoche2017 <timo.fuele@gmail.com>
2017-03-14 09:58:05 +0100

    Update content.php

--- Projektwoche/pages/erstellen/content.php
+++ Projektwoche/pages/erstellen/content.php
@@ -7,10 +7,23 @@
 	<label><b>Projektbeschreibung:</b></label>
 	<textarea name="name" placeholder="Projektbeschreibung"></textarea>
 	<label><b>Angaben:</b></label><br>
-	<input type="text" name="maxS" placeholder="max. Schüler">
-	<input type="text" name="maxJ" style="width: 48.5%;" placeholder="mind. Jahrgangsstufe"> <!-- Dropdown -->
-	-
-	<input type="text" name="maxJ" style="width: 48.5%;" placeholder="max. Jahrgangsstufe">
+	<!--<input type="text" name="maxS" placeholder="max. Schüler"> -->
+	<select name="mind. Jahrgangsstufe">
+		<option value="1">5. Klasse</option>
+		<option value="2">6. Klasse</option>
+		<option value="3">7. Klasse</option>
+		<option value="4">8. Klasse</option>
+		<option value="5">9. Klasse</option>
+		<option value="6">10. Klasse</option>
+	</select>
+	<select name="max. Jahrgangsstufe">
+		<option value="1">5. Klasse</option>
+		<option value="2">6. Klasse</option>
+		<option value="3">7. Klasse</option>
+		<option value="4">8. Klasse</option>
+		<option value="5">9. Klasse</option>
+		<option value="6">10. Klasse</option>
+	</select>
 	<input type="text" name="teacher" placeholder="Lehrer">
 	<button type="submit">Erstellen</button>
 </form>
