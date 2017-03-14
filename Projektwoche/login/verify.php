44fc5e915ac3ca7ff788e9d3bf836d2d46bb7f06
Alexander018018 <alexander@schmied.email>
2017-03-14 10:12:44 +0100

    Funktionalität

--- Projektwoche/login/verify.php
+++ Projektwoche/login/verify.php
@@ -1,21 +1,22 @@
 <?php
 	session_start();
+	require("../databaseFunctionality/LoginCheck/loginCheck.php");
 	$_SESSION['name'] = (isset($_POST['name']))?$_POST['name']:"";
 	if (!empty($_POST['name'])&&!empty($_POST['password'])) {{  
-		$hash = checkPW($_POST['name']); 
-		if (empty($hash)) {		#Wenn schüler nicht in der Datenbank vorhanden..!?	
-			$_SESSION['status'] = 23;	
-		}	
+		$hash = checkPW($_POST['name']);
+		if (!empty($hash[0])) {	
+			$rights = password_verify($_POST['password'], $hash[0])?getRights($_POST['name']):NULL;		
+		}
+		else if (!empty($hash[1])) {
+			$rights = $hash[1]==$_POST['password']?getRights($_POST['name']):NULL;
+		}
+		if ($rights!=NULL) {
+			$_SESSION['id'] = $rights[0];
+			$_SESSION['permission'] = $rights[1];
+			header ("Location: /?page=start");
+		}
 		else {
-			if (password_verify($_POST['password'], $hash)) { 	#Fehlend: Falls nur erstPW vorhanden
-				$rights = getRights($_POST['name']);
-				$_SESSION['id'] = $rights[0];
-				$_SESSION['permission'] = $rights[1];	 #Oder andersrum?
-				header ("Location: /?page=start");
-			}
-			else {
-				$_SESSION['status'] = 32;
-			}
+			$_SESSION['status'] = 32;
 		}
 	}
 	elseif (empty($_POST['name'])&&empty($_POST['password'])){$_SESSION['status'] = 22;}
