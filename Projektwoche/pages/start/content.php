1930f297a0b4e519a8f1b8caa16fdb483eb1d55e
TheRiotSnake <Beckermumi@web.de>
2017-04-25 09:59:42 +0200

    Update content.php

--- Projektwoche/pages/start/content.php
+++ Projektwoche/pages/start/content.php
@@ -5,35 +5,37 @@
 </head>
 <body>
 <section>
-	<h1 class="center" style="padding-left:1em">Willkommen, <?php echo $_SESSION['name'];?>!</h1>
+	<h1 style="padding-left:1em">Willkommen, <?php echo $_SESSION['name'];?>!</h1>
 	<div style="padding: 16px;
 		    border: 3px solid #f1f1f1;
-				max-width: 1008px;
+				width: 1400px;
+				height:auto;
    	    	    margin: auto;">
-	<p>
+	<p style="text-align: center; padding-bottom:40px;">
 		Hier kannst du dich f&uuml;r Projekte eintragen. Bedenke, dass es eine 1., 2. und 3. Wahl gibt.
 	</p>
 	 <img
 	 
-	 style="width:500px; height:500px; padding:0; margin:o;"
+	 style="width:24.5%; height:auto; padding:0px; margin:0px;"
 		     
      src="pics/pic1.jpg" alt="Bild kann nicht geladen werden"></img>
 	 
 	 <img 
 
-	style="width:500px; height:500px; padding:0; margin:0;"
+	style="width:24.5%; height:auto; padding:0px; margin:0px;"
 	
      src="pics/pic2.jpg" alt="Bild kann nicht geladen werden"></img>
 	 
+	 
 	 <img 
 
-	style="width:500px; height:500px; padding:0; margin:0;"
+	style="width:24.5%; height:auto; padding:0px; margin:0px;"
 	
      src="pics/pic3.jpg" alt="Bild kann nicht geladen werden"></img>
 	 
 	 <img 
 
-	style="width:500px; height:500px; padding:0; margin:0;"
+	style="width:24.5%; height:auto; padding:0px; margin:0px;"
 	
      src="pics/pic4.jpg" alt="Bild kann nicht geladen werden"></img>
 	</div>
