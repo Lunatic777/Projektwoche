58ebf44c690298392e7e2f227885db8baec98821
Projektwoche2017 <timo.fuele@gmail.com>
2017-05-02 11:03:44 +0200

    Update content.php

--- Projektwoche/pages/schuelerhilfe/content.php
+++ Projektwoche/pages/schuelerhilfe/content.php
@@ -1,3 +1,7 @@
 <center>
   <h1>Schülerhilfe</h1>
 </center>
+<form class="container">
+  <h2>Änderungen</h2>
+  <label><b>Schüler Name</b></label>
+  <input type="text" name="user" placeholder="Username (nicht Name des Schülers)">
